588a4b8b844c9e4d4f0228cd5b1f0003ed955516
Matheuzinho <grandboss1978@gmail.com>
2025-02-18 23:01:01 -0300

    Adição de 3 novos exercícios completos

---
+++ Exercicios_1/src/Principal16.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Principal16 {
+    public static void main(String[] args) {
+        Scanner ler5 = new Scanner(System.in);
+
+        System.out.print("Insira o valor de largura: ");
+        double largura = ler5.nextDouble();
+        System.out.print("Insira o valor de comprimento: ");
+        double comprimento = ler5.nextDouble();
+
+        double areaSala = largura * comprimento;
+        double lajota = 0.3 * 0.3;
+        double res = areaSala / lajota;
+        res = Math.ceil(res);
+        System.out.printf("A quantidade de lajotas necessárias é: %.0f\n", res);
+
+    ler5.close();
+}}
