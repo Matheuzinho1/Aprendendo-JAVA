fdec4fdbf7a9ee6a0091667a202d7266dd0b4d41
Matheuzinho <grandboss1978@gmail.com>
2025-03-20 01:39:30 -0300

    Adição de 2 exercícios na terceira lista

--- Exercicios_3/src/maiorMenorTodos.java
+++ Exercicios_3/src/maiorMenorTodos.java
@@ -8,19 +8,39 @@ public class maiorMenorTodos {
         
         double[] array = new double[10]; 
         double maior;
+        double menor;
         
         for (int i = 0; i < 10; i++) {
             System.out.printf("Insira o %dº valor: ", i + 1);
             array[i] = ler.nextDouble(); 
         }
 
+        if(array[0] > array[1]) {
+            maior = array[0];
+            menor = array[1];
+        } else {
+            maior = array[1];
+            menor = array[0];
+        }
+
+        for(int i = 2; i < 10; i++) {
+            if(array[i] > maior) {
+                maior = array[i];
+            }
+            if(array[i] < menor) {
+                menor = array[i];
+            }
+        }
+        System.out.println("----------------------");
+        System.out.println("Maior número: " + maior);
+        System.out.println("Menor número: " + menor);
+        System.out.print("Vetor completo: ");
         for(int i = 0; i < 10; i++) {
-        	for(int j = 0; j < i; i++) {
-        		if(array[i] > array[j]) {
-        			maior = array[i];
-        			
-        		}
-        	}
+            if (i != 9) {
+                System.out.print(array[i] + " / ");
+            } else {
+                System.out.print(array[i]);
+            }
         }
         
         ler.close(); 
---
+++ Exercicios_3/src/segundoMaior2.java
@@ -0,0 +1,38 @@
+package Exercicios_3.src;
+
+import java.util.Scanner;
+
+public class segundoMaior2 {
+    public static void main(String[] args) {
+        int array[] = new int[5];
+        int maior = array[0];
+        int sMaior = array[1];
+
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Insira um valor: ");
+
+        for(int i = 0 ; i < 5; i++) {
+            array[i] = ler.nextInt();
+        }
+
+        if (array[0] > array[1]) {
+            maior = array[0];
+            sMaior = array[1];
+        } else {
+            maior = array[1];
+            sMaior = array[0];
+        }
+
+        for(int i = 2; i < 5; i++) {
+            if(array[i] > maior) {
+                sMaior = maior;
+                maior = array[i];
+            } else if(array[i] > sMaior) {
+                sMaior = array[i];
+            }
+        }
+
+        System.out.println(sMaior);
+    }
+}
+
