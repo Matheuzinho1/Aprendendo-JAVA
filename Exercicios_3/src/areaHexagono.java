21871e8eefccf0ab599653849cc86cfa82eaddd1
Matheuzinho <grandboss1978@gmail.com>
2025-03-20 23:26:38 -0300

    Adição de 3 exercícios na terceira lista

---
+++ Exercicios_3/src/areaHexagono.java
@@ -0,0 +1,16 @@
+package Exercicios_3.src;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class areaHexagono {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        DecimalFormat format = new DecimalFormat("#.##");
+
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Olá, insira o lado do hexagono: "));
+        double res = ((3 * Math.sqrt(3)) / 2) * Math.pow(num1, 2);
+        JOptionPane.showMessageDialog(null, format.format(res));
+    }
+}
---
+++ Exercicios_3/src/reverterString.java
@@ -0,0 +1,7 @@
+package Exercicios_3.src;
+
+public class reverterString {
+    public static void main(String[] args) {
+        
+    }
+}
