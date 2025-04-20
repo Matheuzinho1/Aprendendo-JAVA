956085dc18b0d1aca079af15068cb527ca56c928
Matheuzinho <grandboss1978@gmail.com>
2025-04-20 04:01:57 -0300

    Adição de novos exercícios (20/04/2025)

---
+++ Iniciando_com_REPETICAO/src/numeroPrimo2.java
@@ -0,0 +1,28 @@
+package Iniciando_com_REPETICAO.src;
+
+import javax.swing.*;
+
+public class numeroPrimo2 {
+    public static void main(String[] args) {
+        String numString = JOptionPane.showInputDialog(null, "Insira um número", "Calculadora de PRIMO", JOptionPane.QUESTION_MESSAGE);
+
+        int num = Integer.parseInt(numString), i = 1, j = 0;
+
+        while (i < num) {
+            boolean primo = true;
+            j = 2;
+            i++;
+
+            while(j < i) {
+                if(i % j == 0) {
+                    primo = false;
+                    break;
+                }
+                j++;
+            }
+            if(primo) {
+                JOptionPane.showMessageDialog(null, "Números primo: " + i, "Calculadora de PRIMO - Numero: " + num, JOptionPane.QUESTION_MESSAGE);
+            }
+        }
+    }
+}
